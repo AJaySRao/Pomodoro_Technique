dda023d584dc67cf73b5d97cadeca95eb13a8441
changeAJay <ajajay008@gmail.com>
2022-03-06 22:27:37 +0530

    Application's UI, buttons and labels

--- main.py
+++ main.py
@@ -20,13 +20,26 @@ LONG_BREAK_MIN = 20
 
 window = Tk()
 window.title("Pomodoro Technique")
-window.config(padx=100, pady=40, bg=YELLOW)
+window.config(padx=100, pady=60, bg=YELLOW)
+#window.wm_attributes('-transparentcolor', GREEN)
 
 canvas = Canvas(width=200, height=223, bg=YELLOW, highlightthickness=0)
 t_image = PhotoImage(file='tomato.png')
 canvas.create_image(100, 112, image=t_image)
 canvas.create_text(100, 130, text='00:00', font=(FONT_NAME, 35, 'bold'), fill='white' )
-canvas.pack()
+canvas.grid(column=1, row=1)
+
+timer = Label(text='Timer', font=(FONT_NAME, 55, 'bold'), bg=YELLOW, fg=GREEN, highlightthickness=0)
+timer.grid(column=1, row=0)
+
+start = Button(text='Start', highlightthickness=0)
+start.grid(column=0, row=2)
+
+reset = Button(text='Reset', highlightthickness=0)
+reset.grid(column=2, row=2)
+
+done = Label(text='✅', fg='green', bg=YELLOW, highlightthickness=0)
+done.grid(column=1, row=3)
 
 window.mainloop()
 
