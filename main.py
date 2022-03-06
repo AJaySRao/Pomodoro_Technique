c2c39b0d36f979cd72e6ee7aa04afb9f1a7f887a
changeAJay <ajajay008@gmail.com>
2022-03-06 23:21:27 +0530

    COUNTDOWN MECHANISM

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math as m
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -14,8 +15,21 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
+
+def start():
+    count_down(5*60)
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
+
+def count_down(count):
+    min = m.floor(count / 60)
+    sec = count % 60
+    if sec < 10:
+        sec = f'0{sec}'
+
+    canvas.itemconfig(time_text, text=f'{min}:{sec}')
+    if count > 0:
+        window.after(1000, count_down, count-1)
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
@@ -26,13 +40,13 @@ window.config(padx=100, pady=60, bg=YELLOW)
 canvas = Canvas(width=200, height=223, bg=YELLOW, highlightthickness=0)
 t_image = PhotoImage(file='tomato.png')
 canvas.create_image(100, 112, image=t_image)
-canvas.create_text(100, 130, text='00:00', font=(FONT_NAME, 35, 'bold'), fill='white' )
+time_text = canvas.create_text(100, 130, text='00:00', font=(FONT_NAME, 35, 'bold'), fill='white' )
 canvas.grid(column=1, row=1)
 
 timer = Label(text='Timer', font=(FONT_NAME, 55, 'bold'), bg=YELLOW, fg=GREEN, highlightthickness=0)
 timer.grid(column=1, row=0)
 
-start = Button(text='Start', highlightthickness=0)
+start = Button(text='Start', highlightthickness=0, command=start)
 start.grid(column=0, row=2)
 
 reset = Button(text='Reset', highlightthickness=0)
