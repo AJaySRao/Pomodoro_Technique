a43d2fd51a3703b268d234086a9406adb93b1480
changeAJay <ajajay008@gmail.com>
2022-03-07 22:38:02 +0530

    Adding checkMarks and resetting the applications

--- main.py
+++ main.py
@@ -10,26 +10,57 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+rep = 0
+clock = None
 
 # ---------------------------- TIMER RESET ------------------------------- # 
+def re_set():
+    global rep
+    window.after_cancel(clock)
+    timer.config(text='Timer', fg=GREEN)
+    canvas.itemconfig(time_text, text=f'00:00')
+    done.config(text='')
+    rep = 0
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
 
 
-def start():
-    count_down(5*60)
+
+# ---------------------------- TIMER MECHANISM ------------------------------- #
+
+def set_timer():
+    global rep
+    rep += 1
+    work_sec = WORK_MIN * 60
+    sh_sec = SHORT_BREAK_MIN * 60
+    lg_sec = LONG_BREAK_MIN * 60
+
+    if rep % 8 == 0:
+        count_down(lg_sec)
+        timer.config(text='Break', fg=RED)
+    elif rep % 2 == 0:
+        count_down(sh_sec)
+        timer.config(text='Break', fg=PINK)
+    else:
+        count_down(work_sec)
+        timer.config(text='Work', fg=GREEN)
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
-
 def count_down(count):
-    min = m.floor(count / 60)
+    global clock
+    mint = m.floor(count / 60)
     sec = count % 60
     if sec < 10:
         sec = f'0{sec}'
 
-    canvas.itemconfig(time_text, text=f'{min}:{sec}')
+    canvas.itemconfig(time_text, text=f'{mint}:{sec}')
     if count > 0:
-        window.after(1000, count_down, count-1)
+        clock = window.after(1000, count_down, count-1)
+    else:
+        set_timer()
+        mark = ''
+        for _ in range(m.floor(rep/2)):
+            mark += '✅'
+        done.config(text=mark)
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
@@ -46,13 +77,13 @@ canvas.grid(column=1, row=1)
 timer = Label(text='Timer', font=(FONT_NAME, 55, 'bold'), bg=YELLOW, fg=GREEN, highlightthickness=0)
 timer.grid(column=1, row=0)
 
-start = Button(text='Start', highlightthickness=0, command=start)
+start = Button(text='Start', highlightthickness=0, command=set_timer)
 start.grid(column=0, row=2)
 
-reset = Button(text='Reset', highlightthickness=0)
+reset = Button(text='Reset', highlightthickness=0, command=re_set)
 reset.grid(column=2, row=2)
 
-done = Label(text='✅', fg='green', bg=YELLOW, highlightthickness=0)
+done = Label(fg=GREEN, bg=YELLOW, highlightthickness=0)
 done.grid(column=1, row=3)
 
 window.mainloop()
